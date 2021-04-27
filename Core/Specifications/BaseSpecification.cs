4587f9763629527b9d12bf223892a153a0875da6
Antonio <tonymike20@hotmail.com>
2021-04-27 01:10:02 -0600

    End of section 10

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -15,9 +15,7 @@ namespace Core.Specifications
         }
         public Expression<Func<T, bool>> Criteria {get;}
         public List<Expression<Func<T, object>>> Includes {get;} = new List<Expression<Func<T, object>>>();
-
         public Expression<Func<T, object>> OrderBy {get;private set;}
-
         public Expression<Func<T, object>> OrderByDescending {get;private set;}
 
         public int Take {get;private set;}
