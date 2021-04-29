f4c4d3eb1758d4e3143c851e7c157523d63333b8
Antonio <tonymike20@hotmail.com>
2021-04-29 01:37:00 -0600

    End of section 12

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -27,6 +27,11 @@ namespace API.Controllers
 
         [HttpGet("servererror")]
         public ActionResult GetServerError(){
+            
+            var thing = _context.Products.Find(42);
+
+            var thingToReturn = thing.ToString();
+            
             return Ok();
         }
 
