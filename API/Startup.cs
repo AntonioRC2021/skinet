3ab1d8b2ee61cf0898bd991f1807ffadff426720
Antonio <tonymike20@hotmail.com>
2021-05-01 05:13:28 -0600

    End of section 15

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using API.Extensions;
 using API.Helpers;
 using API.Middleware;
 using Infrastructure.Data;
+using Infrastructure.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -21,15 +22,21 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {   
-            services.AddControllers();
             services.AddAutoMapper(typeof(MappingProfiles));
+
+            services.AddControllers();
             services.AddDbContext<StoreContext>(x=>x.UseSqlite(_config.GetConnectionString("DefaultConnection")));   
+            services.AddDbContext<AppIdentityDbContext>(x => {
+                x.UseSqlite(_config.GetConnectionString("IdentityConnection"));
+            });
+
             services.AddSingleton<IConnectionMultiplexer>(c=>{
                 var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"),
                 true);
                 return ConnectionMultiplexer.Connect(configuration);
             });
             services.AddApplicationServices();
+            services.AddIdentityServices(_config);
             services.AddSwaggerDocumentation();
             services.AddCors(opt =>{opt.AddPolicy("CorsPolicy", policy=>{policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
             });
@@ -49,6 +56,7 @@ namespace API
 
             app.UseStaticFiles();
             app.UseCors("CorsPolicy");
+            app.UseAuthentication();
             app.UseAuthorization();
             app.UseSwaggerDocumentation();
 
