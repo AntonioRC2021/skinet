666484c53cbe174c876135d5cb93b1e89dff2fdb
Antonio <tonymike20@hotmail.com>
2021-05-08 22:01:39 -0600

    End of section 20

--- client/src/app/basket/basket.component.ts
+++ client/src/app/basket/basket.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IBasket } from 'src/app/shared/models/basket';
 import { BasketService } from './basket.service';
-import { IBasketItem } from '../shared/models/basket';
+import { IBasketItem, IBasketTotals } from '../shared/models/basket';
 
 @Component({
   selector: 'app-basket',
@@ -11,11 +11,14 @@ import { IBasketItem } from '../shared/models/basket';
 })
 export class BasketComponent implements OnInit {
   basket$: Observable<IBasket>;
+  basketTotals$: Observable<IBasketTotals>;
 
   constructor(private basketService: BasketService) { }
 
   ngOnInit() {
-    this.basket$=this.basketService.basket$;
+    this.basket$ = this.basketService.basket$;
+    this.basketTotals$ = this.basketService.basketTotal$;
+
   }
 
   removeBasketItem(item:IBasketItem){
